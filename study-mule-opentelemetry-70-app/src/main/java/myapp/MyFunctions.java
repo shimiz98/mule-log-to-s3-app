8570d7b352d7a2842b9aac2fa0ec92961ebcd4cc
Yusuke HOMMA <shimiz98@gmail.com>
2025-07-06 18:47:51 +0900

    OpenTelemetryのtraceをAWS X-rayへ転送できた
    * com.avioconsulting.mule:mule-opentelemetry-module:2.9.5
      * ↓
      * ↓ http://localhost:8070/traces
      * ↓ 方式: OTLP/HTTP ※ 「OTLP/gRPC」はAWSが未対応かつ、MuleアプリでListen不可能なため
      * ↓ 認証: なし
    * study-mule-opentelemetry-70-app
      * ↓
      * ↓ https://xray.ap-northeast-1.amazonaws.com/v1/traces
      * ↓ 方式: OTLP/HTTP
      * ↓ 認証: AWS Signature Version 4
    * AWS X-ray

--- study-mule-opentelemetry-70-app/src/main/java/myapp/MyFunctions.java
+++ study-mule-opentelemetry-70-app/src/main/java/myapp/MyFunctions.java
@@ -2,7 +2,9 @@ package myapp;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.nio.charset.StandardCharsets;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.http.AbortableInputStream;
@@ -18,25 +20,25 @@ import software.amazon.awssdk.http.auth.spi.signer.SignedRequest;
 import software.amazon.awssdk.identity.spi.AwsCredentialsIdentity;
 
 public class MyFunctions {
-	public static void sendOpenTelemetory(byte[] data) {
-		sendOpenTelemetory("ap-northeast-1", data);
+	private static final Logger LOG = LoggerFactory.getLogger("MyFunctions");
+	
+	public static byte[] sendTraceToAwsXray(byte[] data) {
+		return sendTraceToAwsXray("ap-northeast-1", data);
 	}
 
-	public static void sendOpenTelemetory(String awsRegion, byte[] data) {
-		sendOpenTelemetory("https://xray." + awsRegion + ".amazonaws.com/v1/traces", awsRegion, data);
+	public static byte[] sendTraceToAwsXray(String awsRegion, byte[] data) {
+		return sendTraceToAwsXray("https://xray." + awsRegion + ".amazonaws.com/v1/traces", awsRegion, data);
 	}
 
-	public static void sendOpenTelemetory(String endpoint, String awsRegion, byte[] data) {
+	public static byte[] sendTraceToAwsXray(String endpoint, String awsRegion, byte[] data) {
+		// https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/CloudWatch-OTLPEndpoint.html
+		// > The endpoint authenticates callers using Signature 4 authentication.
 		// https://qiita.com/cozima/items/84aabaa8f1b827e6b36b
 		// > AWS SDK for Java 2.10.12以前では、AwsS3V4Signerを使用して、Presigned URLを生成していました
 		// https://github.com/aws/aws-sdk-java-v2/blob/master/core/auth/src/main/java/software/amazon/awssdk/auth/signer/Aws4Signer.java
-		// > @deprecated Use {@code
-		// software.amazon.awssdk.http.auth.aws.signer.AwsV4HttpSigner} from the
-		// 'http-auth-aws' module.
+		// > @deprecated Use {@code software.amazon.awssdk.http.auth.aws.signer.AwsV4HttpSigner} from the 'http-auth-aws' module.
 		// https://sdk.amazonaws.com/java/api/latest/software/amazon/awssdk/http/auth/aws/signer/AwsV4HttpSigner.html
 		// > Using the AwsV4HttpSigner
-		// https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/CloudWatch-OTLPEndpoint.html
-		// >
 		AwsV4HttpSigner signer = AwsV4HttpSigner.create();
 
 		// Specify AWS credentials. Credential providers that are used by the SDK by
@@ -53,7 +55,7 @@ public class MyFunctions {
 		// Create the HTTP request to be signed
 		SdkHttpRequest httpRequest = SdkHttpRequest.builder().uri(endpoint).method(SdkHttpMethod.POST)
 				.putHeader("Content-Type", "application/x-protobuf").build();
-		// TODO add http header logGourp & logStream
+		// TODO 1回のリクエスト当たりの最大個数と最大byte数を超えていたら、分割して送信する。
 
 		// Create the request payload to be signed
 		ContentStreamProvider requestPayload = //
@@ -78,29 +80,41 @@ public class MyFunctions {
 
 			HttpExecuteResponse httpResponse = httpClient.prepareRequest(httpExecuteRequest).call();
 
-			System.out.println("HTTP Status Code: " + httpResponse.httpResponse().statusCode());
+			LOG.info("HTTP Status Code: {}", httpResponse.httpResponse().statusCode());
 			int httpStatuCode = httpResponse.httpResponse().statusCode();
 			if (httpStatuCode != 200) {
+				LOG.error("HTTP Status Code: {} {}", httpResponse.httpResponse().statusCode(), toString(httpResponse));
 				throw new RuntimeException("httpStatuCode=" + httpStatuCode + "\n" + toString(httpResponse));
 			}
 		} catch (IOException e) {
 			System.err.println("HTTP Request Failed.");
-			e.printStackTrace();
 			throw new UncheckedIOException(e);
 		}
+		
+		// 以下の対策として、返却する。
+		// "Cannot coerce Null (null) to Binary" evaluating expression: "java!myapp::MyFunctions::sendTraceToAwsXray(payload as Binary) as Null".
+		return data;
 	}
 
 	static String toString(HttpExecuteResponse res) throws IOException {
-		// protbuf形式なので、先頭に3byte付く。
-		// [8, 7, 18, 54, 84, 
-		// 6The security token included in the request is invalid.
-		byte [] resBodyBytes = res.responseBody().orElse(AbortableInputStream.createEmpty()).readAllBytes();
-		String resBodyString = new String(resBodyBytes, StandardCharsets.ISO_8859_1);
-
 		StringBuilder sb = new StringBuilder() //
 				.append("httpStatusCode=").append(res.httpResponse().statusCode()) //
 				.append(" httpStatusText=").append(res.httpResponse().statusText()) //
-				.append(" httpResBody=").append(resBodyString);
+				.append(" httpResBody=");
+
+		// protbuf形式なことから先頭に数byteのバイナリデータが付くので、エスケープ出力する。
+		// [8, 7, 18, 54, 84, 
+		// 6The security token included in the request is invalid.
+		byte [] resBodyBytes = res.responseBody().orElse(AbortableInputStream.createEmpty()).readAllBytes();
+		// String resBodyString = new String(resBodyBytes, StandardCharsets.ISO_8859_1);
+		for (int i = 0; i < resBodyBytes.length; i++) {
+			int c = (int) resBodyBytes[i] & 0xff;
+			if (' ' <= c && c <= '~') {
+				sb.append((char)c);
+			} else {
+				sb.append("\\%02x".formatted(c));
+			}
+		}
 		return sb.toString();
 	}
 }
